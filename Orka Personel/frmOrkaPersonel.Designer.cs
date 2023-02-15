9409feb13b0143b292d0e561b3a607379ecdcc11
Buğra Demirtaş <75027268+BugraDemirtas@users.noreply.github.com>
2023-02-15 10:42:39 +0300

    Add files via upload

---
+++ Orka Personel/frmOrkaPersonel.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace Orka_Personel
+{
+    partial class frmOrkaPersonel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rchSonuc = new System.Windows.Forms.RichTextBox();
+            this.butGetAllEmployees = new System.Windows.Forms.Button();
+            this.butGetAllPermit = new System.Windows.Forms.Button();
+            this.butGetAllPayment = new System.Windows.Forms.Button();
+            this.butAddPermit = new System.Windows.Forms.Button();
+            this.butGetAllJoinedEmployees = new System.Windows.Forms.Button();
+            this.butGetAllLeftedEmployees = new System.Windows.Forms.Button();
+            this.butGetAllPersonPermit = new System.Windows.Forms.Button();
+            this.stripDurum = new System.Windows.Forms.StatusStrip();
+            this.StatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.stripDurum.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rchSonuc
+            // 
+            this.rchSonuc.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rchSonuc.Location = new System.Drawing.Point(348, 12);
+            this.rchSonuc.Name = "rchSonuc";
+            this.rchSonuc.Size = new System.Drawing.Size(463, 623);
+            this.rchSonuc.TabIndex = 3;
+            this.rchSonuc.Text = "";
+            // 
+            // butGetAllEmployees
+            // 
+            this.butGetAllEmployees.BackColor = System.Drawing.Color.PeachPuff;
+            this.butGetAllEmployees.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.butGetAllEmployees.Location = new System.Drawing.Point(12, 12);
+            this.butGetAllEmployees.Name = "butGetAllEmployees";
+            this.butGetAllEmployees.Size = new System.Drawing.Size(100, 101);
+            this.butGetAllEmployees.TabIndex = 16;
+            this.butGetAllEmployees.Text = "PERSONEL LİSTESİ ";
+            this.butGetAllEmployees.UseVisualStyleBackColor = false;
+            this.butGetAllEmployees.Click += new System.EventHandler(this.butGetAllEmployees_Click);
+            // 
+            // butGetAllPermit
+            // 
+            this.butGetAllPermit.BackColor = System.Drawing.Color.PeachPuff;
+            this.butGetAllPermit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.butGetAllPermit.Location = new System.Drawing.Point(118, 12);
+            this.butGetAllPermit.Name = "butGetAllPermit";
+            this.butGetAllPermit.Size = new System.Drawing.Size(100, 101);
+            this.butGetAllPermit.TabIndex = 17;
+            this.butGetAllPermit.Text = "İZİN LİSTESİ";
+            this.butGetAllPermit.UseVisualStyleBackColor = false;
+            this.butGetAllPermit.Click += new System.EventHandler(this.butGetAllPermit_Click);
+            // 
+            // butGetAllPayment
+            // 
+            this.butGetAllPayment.BackColor = System.Drawing.Color.PeachPuff;
+            this.butGetAllPayment.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.butGetAllPayment.Location = new System.Drawing.Point(224, 12);
+            this.butGetAllPayment.Name = "butGetAllPayment";
+            this.butGetAllPayment.Size = new System.Drawing.Size(100, 101);
+            this.butGetAllPayment.TabIndex = 18;
+            this.butGetAllPayment.Text = "ÖDEMELER LİSTESİ";
+            this.butGetAllPayment.UseVisualStyleBackColor = false;
+            this.butGetAllPayment.Click += new System.EventHandler(this.butGetAllPayment_Click);
+            // 
+            // butAddPermit
+            // 
+            this.butAddPermit.BackColor = System.Drawing.Color.PeachPuff;
+            this.butAddPermit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.butAddPermit.Location = new System.Drawing.Point(118, 226);
+            this.butAddPermit.Name = "butAddPermit";
+            this.butAddPermit.Size = new System.Drawing.Size(100, 101);
+            this.butAddPermit.TabIndex = 19;
+            this.butAddPermit.Text = "İZİN EKLE";
+            this.butAddPermit.UseVisualStyleBackColor = false;
+            this.butAddPermit.Click += new System.EventHandler(this.butAddPermit_Click);
+            // 
+            // butGetAllJoinedEmployees
+            // 
+            this.butGetAllJoinedEmployees.BackColor = System.Drawing.Color.PeachPuff;
+            this.butGetAllJoinedEmployees.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.butGetAllJoinedEmployees.Location = new System.Drawing.Point(12, 119);
+            this.butGetAllJoinedEmployees.Name = "butGetAllJoinedEmployees";
+            this.butGetAllJoinedEmployees.Size = new System.Drawing.Size(100, 101);
+            this.butGetAllJoinedEmployees.TabIndex = 20;
+            this.butGetAllJoinedEmployees.Text = "İŞE BAŞLAYAN PERSONEL LİSTESİ";
+            this.butGetAllJoinedEmployees.UseVisualStyleBackColor = false;
+            this.butGetAllJoinedEmployees.Click += new System.EventHandler(this.butGetAllJoinedEmployees_Click);
+            // 
+            // butGetAllLeftedEmployees
+            // 
+            this.butGetAllLeftedEmployees.BackColor = System.Drawing.Color.PeachPuff;
+            this.butGetAllLeftedEmployees.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.butGetAllLeftedEmployees.Location = new System.Drawing.Point(12, 226);
+            this.butGetAllLeftedEmployees.Name = "butGetAllLeftedEmployees";
+            this.butGetAllLeftedEmployees.Size = new System.Drawing.Size(100, 101);
+            this.butGetAllLeftedEmployees.TabIndex = 21;
+            this.butGetAllLeftedEmployees.Text = "İŞTEN ÇIKAN PERSONEL LİSTESİ";
+            this.butGetAllLeftedEmployees.UseVisualStyleBackColor = false;
+            this.butGetAllLeftedEmployees.Click += new System.EventHandler(this.butGetAllLeftedEmployees_Click);
+            // 
+            // butGetAllPersonPermit
+            // 
+            this.butGetAllPersonPermit.BackColor = System.Drawing.Color.PeachPuff;
+            this.butGetAllPersonPermit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.butGetAllPersonPermit.Location = new System.Drawing.Point(118, 119);
+            this.butGetAllPersonPermit.Name = "butGetAllPersonPermit";
+            this.butGetAllPersonPermit.Size = new System.Drawing.Size(100, 101);
+            this.butGetAllPersonPermit.TabIndex = 22;
+            this.butGetAllPersonPermit.Text = "İZİN TALEP LİSTESİ";
+            this.butGetAllPersonPermit.UseVisualStyleBackColor = false;
+            this.butGetAllPersonPermit.Click += new System.EventHandler(this.butGetAllPersonPermit_Click);
+            // 
+            // stripDurum
+            // 
+            this.stripDurum.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusLabel});
+            this.stripDurum.Location = new System.Drawing.Point(0, 638);
+            this.stripDurum.Name = "stripDurum";
+            this.stripDurum.Size = new System.Drawing.Size(823, 22);
+            this.stripDurum.TabIndex = 23;
+            this.stripDurum.Text = "statusStrip1";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(0, 17);
+            // 
+            // frmOrkaPersonel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 660);
+            this.Controls.Add(this.stripDurum);
+            this.Controls.Add(this.butGetAllPersonPermit);
+            this.Controls.Add(this.butGetAllLeftedEmployees);
+            this.Controls.Add(this.butGetAllJoinedEmployees);
+            this.Controls.Add(this.butAddPermit);
+            this.Controls.Add(this.butGetAllPayment);
+            this.Controls.Add(this.butGetAllPermit);
+            this.Controls.Add(this.butGetAllEmployees);
+            this.Controls.Add(this.rchSonuc);
+            this.Name = "frmOrkaPersonel";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ORKA PERSONEL";
+            this.Load += new System.EventHandler(this.frmOrkaPersonel_Load);
+            this.stripDurum.ResumeLayout(false);
+            this.stripDurum.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rchSonuc;
+        private System.Windows.Forms.Button butGetAllEmployees;
+        private System.Windows.Forms.Button butGetAllPermit;
+        private System.Windows.Forms.Button butGetAllPayment;
+        private System.Windows.Forms.Button butAddPermit;
+        private System.Windows.Forms.Button butGetAllJoinedEmployees;
+        private System.Windows.Forms.Button butGetAllLeftedEmployees;
+        private System.Windows.Forms.Button butGetAllPersonPermit;
+        private System.Windows.Forms.StatusStrip stripDurum;
+        private System.Windows.Forms.ToolStripStatusLabel StatusLabel;
+    }
+}
+
---
+++ Orka Personel/frmOrkaPersonel.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Orka_Token.Class;
+using OrkaToken.Class;
+using OrkaToken.Model;
+using OrkaToken.Model.DTO.Personel;
+using RestSharp;
+
+
+namespace Orka_Personel
+{
+    public partial class frmOrkaPersonel : Form
+    {
+
+        #region Constructor
+
+        // BU ALANA ORKA LOGIN KISMINDAN ALDIĞINIZ VEYA KENDİ METHODLARINIZLA ALDIĞINIZ TOKENİ VEREBİLİRSİNİZ.
+        public string Token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJPcmthRGVtbyIsInVuaXF1ZV9uYW1lIjoiT1JLQV8wMDAxXzIwMjIiLCJyb2xlIjoiQWRtaW4iLCJGaXJtYUtvZHUiOiJPUktBXzAwMDFfMjAyMiIsIkNvbm5lY3Rpb24iOiIiLCJFbnRlZ3JlQXBwIjoiT1JLQSDDlnJuZWsgUHJvamUiLCJFbnRlZ3JlVXNlciI6Ik9SS0Egw5ZybmVrIEt1bCIsIlNlc3Npb25HdWlkIjoiMDAwMDAwMDAtMDAwMC0wMDAwLTAwMDAtMDAwMDAwMDAwMDAwIiwiS2V5IjoiRC1FLU0tTyIsIm5iZiI6MTY0MzE4MDQ3NiwiZXhwIjoxNjQzMTg0MDc2LCJpYXQiOjE2NDMxODA0NzZ9.DRfOAqyV-jc8BSdyAAWiYQFcwTLoB9zE2f0MBTR-KeM";
+
+        public frmOrkaPersonel()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Personel
+
+        private void butGetAllEmployees_Click(object sender, EventArgs e)
+        {
+            RestClient Client = new RestClient("https://api.orka.com.tr/");
+
+            //Request (istek) için oluşturuluyor içine data almak istediğiniz methodun url yazıyorsunuz 
+            var Request = new RestRequest("HRM/Reports/GetAllEmployees");
+
+            //Request (istek) için method seçiliyor (Post,Get Vb.)
+            Request.Method = Method.Get;
+
+            //Request için QueryParametresi Ekleniyor
+            Request.AddQueryParameter("Page", "1");
+
+            //Request için header içine yetki bilgileri ekleniyor. Bearer 
+            //Request için header içine dönüş tipi bilgileri ekleniyor 
+            Request.AddHeader("Authorization", Token);
+            Request.AddHeader("accept", "application/json");
+
+
+            //Response (cevap) için oluşturuluyor içine Request(istek) gönderiliyor. 
+            //OrkaResponse tüm dönüşlerde olan ana bir modeldir . Data kısmına ise kendi olutşurduğunuz modeli gönderebilirsiniz örneğin OrkaResponseModel<OLUSTURULAN MODEL>
+            var Response = Client.ExecuteAsync<OrkaResponseModel<IEnumerable<PersonelListDTO>>>(Request).Result;
+
+            //Response ekranda görüntülemek için yapıldı zorunlu değil
+            rchSonuc.Text = Response.Data.ToJson();
+
+            //Statüs çubugu güncellemesi için yapıldı -- Zorunlu değil 
+            if (string.IsNullOrWhiteSpace(Response.Data.ErrorCodeDescription))
+            {
+                StatusLabel.Text = $"Personel listesi alındı. Kayıt Sayısı : {Response.Data.RecordSize}";
+            }
+        }
+
+        private void butGetAllJoinedEmployees_Click(object sender, EventArgs e)
+        {
+            RestClient Client = new RestClient("https://api.orka.com.tr/");
+
+            //Request (istek) için oluşturuluyor içine data almak istediğiniz methodun url yazıyorsunuz 
+            var Request = new RestRequest("HRM/Reports/GetAllJoinedEmployees");
+
+            //Request (istek) için method seçiliyor (Post,Get Vb.)
+            Request.Method = Method.Get;
+
+            //Request için QueryParametresi Ekleniyor
+            Request.AddQueryParameter("startJoinDate", "01.01.2022");
+            Request.AddQueryParameter("finishJoinDate", "31.01.2022");
+
+            //Request için header içine yetki bilgileri ekleniyor. Bearer 
+            //Request için header içine dönüş tipi bilgileri ekleniyor 
+            Request.AddHeader("Authorization", Token);
+            Request.AddHeader("accept", "application/json");
+
+
+            //Response (cevap) için oluşturuluyor içine Request(istek) gönderiliyor. 
+            //OrkaResponse tüm dönüşlerde olan ana bir modeldir . Data kısmına ise kendi olutşurduğunuz modeli gönderebilirsiniz örneğin OrkaResponseModel<OLUSTURULAN MODEL>
+            var Response = Client.ExecuteAsync<OrkaResponseModel<IEnumerable<PersonelListDTO>>>(Request).Result;
+
+            //Response ekranda görüntülemek için yapıldı zorunlu değil
+            rchSonuc.Text = Response.Data.ToJson();
+
+            //Statüs çubugu güncellemesi için yapıldı -- Zorunlu değil 
+            if (string.IsNullOrWhiteSpace(Response.Data.ErrorCodeDescription))
+            {
+                StatusLabel.Text = $"İşe başlayan personel listesi alındı. Kayıt Sayısı : {Response.Data.RecordSize}";
+            }
+        }
+
+        private void butGetAllLeftedEmployees_Click(object sender, EventArgs e)
+        {
+            RestClient Client = new RestClient("https://api.orka.com.tr/");
+
+            //Request (istek) için oluşturuluyor içine data almak istediğiniz methodun url yazıyorsunuz 
+            var Request = new RestRequest("HRM/Reports/GetAllLeftedEmployees");
+
+            //Request (istek) için method seçiliyor (Post,Get Vb.)
+            Request.Method = Method.Get;
+
+            //Request için QueryParametresi Ekleniyor
+            Request.AddQueryParameter("startLeavingDate", "01.01.2022");
+            Request.AddQueryParameter("finishLeavingDate", "31.01.2022");
+
+            //Request için header içine yetki bilgileri ekleniyor. Bearer 
+            //Request için header içine dönüş tipi bilgileri ekleniyor 
+            Request.AddHeader("Authorization", Token);
+            Request.AddHeader("accept", "application/json");
+
+
+            //Response (cevap) için oluşturuluyor içine Request(istek) gönderiliyor. 
+            //OrkaResponse tüm dönüşlerde olan ana bir modeldir . Data kısmına ise kendi olutşurduğunuz modeli gönderebilirsiniz örneğin OrkaResponseModel<OLUSTURULAN MODEL>
+            var Response = Client.ExecuteAsync<OrkaResponseModel<IEnumerable<PersonelListDTO>>>(Request).Result;
+
+            //Response ekranda görüntülemek için yapıldı zorunlu değil
+            rchSonuc.Text = Response.Data.ToJson();
+
+            //Statüs çubugu güncellemesi için yapıldı -- Zorunlu değil 
+            if (string.IsNullOrWhiteSpace(Response.Data.ErrorCodeDescription))
+            {
+                StatusLabel.Text = $"İşten çıkan personel listesi alındı. Kayıt Sayısı : {Response.Data.RecordSize}";
+            }
+        }
+
+        #endregion
+
+        #region İzin
+
+        private void butGetAllPermit_Click(object sender, EventArgs e)
+        {
+            RestClient Client = new RestClient("https://api.orka.com.tr/");
+
+            //Request (istek) için oluşturuluyor içine data almak istediğiniz methodun url yazıyorsunuz 
+            var Request = new RestRequest("HRM/Operations/Permits/GetAllPermit");
+
+            //Request (istek) için method seçiliyor (Post,Get Vb.)
+            Request.Method = Method.Get;
+
+            //Request için QueryParametresi Ekleniyor
+            Request.AddQueryParameter("Page", "1");
+
+            //Request için header içine yetki bilgileri ekleniyor. Bearer 
+            //Request için header içine dönüş tipi bilgileri ekleniyor 
+            Request.AddHeader("Authorization", Token);
+            Request.AddHeader("accept", "application/json");
+
+
+            //Response (cevap) için oluşturuluyor içine Request(istek) gönderiliyor. 
+            //OrkaResponse tüm dönüşlerde olan ana bir modeldir . Data kısmına ise kendi olutşurduğunuz modeli gönderebilirsiniz örneğin OrkaResponseModel<OLUSTURULAN MODEL>
+            var Response = Client.ExecuteAsync<OrkaResponseModel<IEnumerable<PersonelIzinListDTO>>>(Request).Result;
+
+            //Response ekranda görüntülemek için yapıldı zorunlu değil
+            rchSonuc.Text = Response.Data.ToJson();
+
+            //Statüs çubugu güncellemesi için yapıldı -- Zorunlu değil 
+            if (string.IsNullOrWhiteSpace(Response.Data.ErrorCodeDescription))
+            {
+                StatusLabel.Text = $"Personel izin listesi alındı. Kayıt Sayısı : {Response.Data.RecordSize}";
+            }
+        }
+
+        private void butAddPermit_Click(object sender, EventArgs e)
+        {
+            RestClient Client = new RestClient("https://api.orka.com.tr/");
+
+            //Request (istek) için oluşturuluyor içine data almak istediğiniz methodun url yazıyorsunuz 
+            var Request = new RestRequest("HRM/Operations/Permits/AddPermit");
+
+            //Request (istek) için method seçiliyor (Post,Get Vb.)
+            Request.Method = Method.Post;
+
+            //Request için header içine yetki bilgileri ekleniyor. Bearer 
+            //Request için header içine dönüş tipi bilgileri ekleniyor 
+            Request.AddHeader("Authorization", Token);
+            Request.AddHeader("accept", "application/json");
+
+
+            //Request içinde body olarak request modeli oluşturuyoruz
+            //Örnek Model jsonları için https://api.orka.com.tr/doc bakabilirsiniz .
+            var Izin = new PersonelIzinEkleDTO();
+            {
+                Izin.tcKimlikNo = "52087578866";
+                Izin.isegiriscikis_kayitdamga = "01C795D0-9BAE-4D87-A04A-06EE4BAF41B4";
+                Izin.FirstDate = new DateTime(2022, 01, 15);
+                Izin.EndDate = new DateTime(2022, 01, 20);
+                Izin.WorkStartingDate = new DateTime(2022, 01, 21);
+                Izin.DayCount = 5;
+                Izin.WorkType = Enums.TIC_PUB_CALISMATIP.Yillik;
+            }
+
+            //Oluşturulan modeli Request içine ekliyoruz 
+            Request.AddJsonBody(Izin);
+
+
+
+            //Response (cevap) için oluşturuluyor içine Request(istek) gönderiliyor. 
+            //OrkaResponse tüm dönüşlerde olan ana bir modeldir . Data kısmına ise kendi olutşurduğunuz modeli gönderebilirsiniz örneğin OrkaResponseModel<OLUSTURULAN MODEL>
+            var Response = Client.ExecuteAsync<OrkaResponseModel<IEnumerable<PersonelIzinListDTO>>>(Request).Result;
+
+            //Response ekranda görüntülemek için yapıldı zorunlu değil
+            rchSonuc.Text = Response.Data.ToJson();
+        }
+
+        private void butGetAllPersonPermit_Click(object sender, EventArgs e)
+        {
+            RestClient Client = new RestClient("https://api.orka.com.tr/");
+
+            //Request (istek) için oluşturuluyor içine data almak istediğiniz methodun url yazıyorsunuz 
+            var Request = new RestRequest("HRM/Operations/Permits/GetAllPersonPermits");
+
+            //Request (istek) için method seçiliyor (Post,Get Vb.)
+            Request.Method = Method.Get;
+
+            //Request için QueryParametresi Ekleniyor
+            Request.AddQueryParameter("Page", "1");
+
+            //Request için header içine yetki bilgileri ekleniyor. Bearer 
+            //Request için header içine dönüş tipi bilgileri ekleniyor 
+            Request.AddHeader("Authorization", Token);
+            Request.AddHeader("accept", "application/json");
+
+
+            //Response (cevap) için oluşturuluyor içine Request(istek) gönderiliyor. 
+            //OrkaResponse tüm dönüşlerde olan ana bir modeldir . Data kısmına ise kendi olutşurduğunuz modeli gönderebilirsiniz örneğin OrkaResponseModel<OLUSTURULAN MODEL>
+            var Response = Client.ExecuteAsync<OrkaResponseModel<IEnumerable<PersonelIzinListDTO>>>(Request).Result;
+
+            //Response ekranda görüntülemek için yapıldı zorunlu değil
+            rchSonuc.Text = Response.Data.ToJson();
+
+            //Statüs çubugu güncellemesi için yapıldı -- Zorunlu değil 
+            if (string.IsNullOrWhiteSpace(Response.Data.ErrorCodeDescription))
+            {
+                StatusLabel.Text = $"Personel izin talep listesi alındı. Kayıt Sayısı : {Response.Data.RecordSize}";
+            }
+        }
+
+        #endregion
+
+        #region Ödeme
+
+        private void butGetAllPayment_Click(object sender, EventArgs e)
+        {
+            RestClient Client = new RestClient("https://api.orka.com.tr/");
+
+            //Request (istek) için oluşturuluyor içine data almak istediğiniz methodun url yazıyorsunuz 
+            var Request = new RestRequest("HRM/Reports/GetAllPayment");
+
+            //Request (istek) için method seçiliyor (Post,Get Vb.)
+            Request.Method = Method.Get;
+
+            //Request için QueryParametresi Ekleniyor
+            Request.AddQueryParameter("Page", "1");
+
+            //Request için header içine yetki bilgileri ekleniyor. Bearer 
+            //Request için header içine dönüş tipi bilgileri ekleniyor 
+            Request.AddHeader("Authorization", Token);
+            Request.AddHeader("accept", "application/json");
+
+
+            //Response (cevap) için oluşturuluyor içine Request(istek) gönderiliyor. 
+            //OrkaResponse tüm dönüşlerde olan ana bir modeldir . Data kısmına ise kendi olutşurduğunuz modeli gönderebilirsiniz örneğin OrkaResponseModel<OLUSTURULAN MODEL>
+            var Response = Client.ExecuteAsync<OrkaResponseModel<IEnumerable<PersonelOdemeListDTO>>>(Request).Result;
+
+            //Response ekranda görüntülemek için yapıldı zorunlu değil
+            rchSonuc.Text = Response.Data.ToJson();
+
+            //Statüs çubugu güncellemesi için yapıldı -- Zorunlu değil 
+            if (string.IsNullOrWhiteSpace(Response.Data.ErrorCodeDescription))
+            {
+                StatusLabel.Text = $"Personel icra listesi alındı. Kayıt Sayısı : {Response.Data.RecordSize}";
+            }
+        }
+        
+        #endregion
+
+        #region Load
+        //Ana formdan gelen token class içinden eşitleniyor
+        private void frmOrkaPersonel_Load(object sender, EventArgs e)
+        {
+            Token = MainToken.mainToken;
+        }
+
+        #endregion
+
+    }
+}
